a44a94764be9a24326f91a34c333605723edd428
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-06-21 17:57:59 -0300

    flag brazil change

--- src/Components/LoginArea/Top.jsx
+++ src/Components/LoginArea/Top.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import imageBrasil from '../../Images/imageBrasil.png'
-
+import brasilFlag from '../../Images/brasilFlag.jpg'
 
 export default function Top() {
   return (
-    <div className='bg-gray-200 border-t-2 border-black p-4'>
+    <div className='bg-gray-200 border-t-2 border-black p-12'>
        <div className='flex flex-col lg:flex-row md:flex-row justify-center items-center'>
           <div className='flex flex-col lg:flex-row md:flex-row justify-center items-center'>
-          <img className='w-44' src={imageBrasil} alt="" />
+          <img className='w-32' src={brasilFlag} alt="" />
           <div className='ml-5 text-center lg:text-left md:text-left'>
-          <h2 className='text-2xl font-bold text-[#3572eb]'>Programa de Qualificação Profissional às Pessoas Privadas de Liberdade</h2>
+          <h2 className='text-2xl font-bold text-[#4169E1]'>Programa de Qualificação Profissional às Pessoas Privadas de Liberdade</h2>
           <p className='text-lg font-semibold text-[#315DAD]'>Legislação aplicada: <i>Art. 126 da Lei de Execução Penal e Resolução nº 391/2021-CNJ</i></p>
           </div>
           <div className='mt-10 lg:mt-0 lg:ml-20 text-[#315DAD] text-lg font-semibold'>
