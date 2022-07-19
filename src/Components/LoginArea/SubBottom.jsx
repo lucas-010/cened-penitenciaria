797dcaefa02cb71fcc31202ce05a4ea9f6988d30
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-07-19 14:59:46 -0300

    image bridge height fixed

--- src/Components/LoginArea/SubBottom.jsx
+++ src/Components/LoginArea/SubBottom.jsx
@@ -4,7 +4,7 @@ import dfFlag from '../../Images/dfFlag.png'
 
 export default function SubBottom() {
   return (
-    <div className='w-full h-96' style={{background: `url(${ponteJk}), rgba(0, 0, 0, 0.7)`,backgroundSize: 'cover', backgroundPosition: 'center',backgroundRepeat: 'no-repeat', backgroundBlendMode: 'darken'}}>
+    <div className='w-full' style={{background: `url(${ponteJk}), rgba(0, 0, 0, 0.7)`,backgroundSize: 'cover', backgroundPosition: 'center',backgroundRepeat: 'no-repeat', backgroundBlendMode: 'darken', height: '400px'}}>
         <div className='flex items-center lg:items-start p-4 flex-col'>
             <img className='w-28' src={dfFlag} alt="" />
             <p className='text-white ml-1 text-xl'>Brasília - DF</p>
