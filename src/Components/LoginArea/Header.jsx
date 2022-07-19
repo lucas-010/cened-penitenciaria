80d398a25d8a2246761383935008a52288cc2f4c
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-07-19 15:01:55 -0300

    link logo removed

--- src/Components/LoginArea/Header.jsx
+++ src/Components/LoginArea/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import wavesBlue from '../../Images/1.jpg'
 import logoCened from '../../Images/logoCened.png'
-import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div style={{backgroundImage: `url(${wavesBlue})`, backgroundSize: 'cover'}} className='flex justify-center lg:justify-between items-center h-20 w-full font-bold'>
       <div className='lg:ml-5 md:ml-5 ml-0'>
-        <Link to={'/CadastroDeAlunos'}><img src={logoCened} className='h-20' alt="" /></Link>
+        <img src={logoCened} className='h-20' alt="" />
       </div>
 
       <div className='lg:block md:block hidden mt-5 mr-2'>
