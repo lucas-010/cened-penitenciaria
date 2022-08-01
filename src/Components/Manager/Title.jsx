b3f6aa1bc29b2912edd2b2db640be54a462bbab0
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-08-01 17:53:19 -0300

    title margin top print fixed

--- src/Components/Manager/Title.jsx
+++ src/Components/Manager/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Title() {
   return (
-    <div className='w-full text-center mt-10 font-semibold text-xl text-[#594ae1]'>
+    <div className='w-full text-center mt-10 print:mt-0 font-semibold text-xl text-[#594ae1]'>
         <h2>FISCALIZAÇÃO E CONTROLE DAS SECRETARIAS DE ADMINISTRAÇÃO PENITENCIÁRIAS DOS ESTADOS</h2>
     </div>
   )
