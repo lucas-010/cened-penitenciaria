d92fe33ff1a0fd9fe2fab435a6152e6b9e28f9d8
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-08-01 17:55:53 -0300

    header print hidden

--- src/Components/Manager/Header.jsx
+++ src/Components/Manager/Header.jsx
@@ -3,7 +3,7 @@ import {FaBookReader} from 'react-icons/fa'
 
 export default function Header() {
   return (
-    <div className='bg-[#594ae1] w-full p-4'>
+    <div className='bg-[#594ae1] print:hidden w-full p-4'>
         <div className='text-white flex items-center text-xl'>
             <FaBookReader size={30}/>
             <h1 className='ml-2'>CENED Qualificando</h1>
